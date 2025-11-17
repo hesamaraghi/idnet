db1e6d2cda5243ca943696f3888bfeba1079dc55
hesamaraghi <hesam.araghi@gmail.com>
2025-11-17 20:49:56 +0100

    Add nano IDNET model

--- idn/model/update.py
+++ idn/model/update.py
@@ -91,3 +91,11 @@ class TinyUpdateBlock(LiteUpdateBlock):
             nn.Conv2d(hidden_dim, 128, 3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, self.upsample_mask_dim*9, 1, padding=0))
+        
+class NanoUpdateBlock(LiteUpdateBlock):
+    def __init__(self, hidden_dim=16, input_dim=8, num_outputs=1, downsample=8):
+        super(NanoUpdateBlock, self).__init__(hidden_dim=hidden_dim, input_dim=input_dim, num_outputs=num_outputs, downsample=downsample)
+        self.mask = nn.Sequential(
+            nn.Conv2d(hidden_dim, 16, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, self.upsample_mask_dim*9, 1, padding=0))
