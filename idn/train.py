777f669e79e30bd68940884f255c469b2876c8aa
hesamaraghi <hesam.araghi@gmail.com>
2026-01-28 10:34:28 +0100

    add resume to train.py for subdataset DSEC

--- idn/train.py
+++ idn/train.py
@@ -11,6 +11,24 @@ def main(config):
     cmd_cfg = OmegaConf.from_cli()
     config = OmegaConf.merge(config, cmd_cfg)
     
+    if config.get("resume_ckpt", None):
+        print(f"Try reading config from wandb server for resume...")
+        try:
+            import wandb
+            api = wandb.Api()
+            # extract run path from resume checkpoint path
+            run_path = "/".join(["haraghi",config.wandb.project, config.resume_ckpt.split("/")[-2]])
+            run = api.run(run_path)
+            config_dict = dict(run.config)
+            wandb_omega_config = OmegaConf.create(config_dict)
+            wandb_omega_config.resume_ckpt = config.resume_ckpt
+            config = OmegaConf.merge(config, wandb_omega_config)
+            print(f"Successfully read config from wandb server for resume.")
+        except Exception as e:
+            print(f"Failed to read config from wandb server for resume. Exception: {e}")
+            
+    
+    config = OmegaConf.merge(config, cmd_cfg)
     print(OmegaConf.to_yaml(config))
 
     trainer = Trainer(config)
